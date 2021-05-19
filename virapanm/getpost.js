cca2bc84d2aeac606e10e150dba5e50eeed1e39a
mehavira <71848177+mehavira@users.noreply.github.com>
2021-05-18 22:02:41 -0700

    Add files via upload

---
+++ virapanm/getpost.js
@@ -0,0 +1,59 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 1234);
+
+app.get('/',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.requestType = 'GET';
+  context.showTable = false;
+  context.dataList1 = qParams;
+  context.dataList2 = [];
+  res.render('tables', context);
+});
+
+app.post('/', function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.requestType = 'POST';
+  context.showTable = true;
+  context.dataList1 = qParams;
+
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  context.dataList2 = qParams;
+  res.render('tables', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
